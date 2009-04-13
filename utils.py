32550dd1e936da227b5546b127e76b0c993afc20
Curt Micol <cbm@dragn.projectcloud.com>
2009-04-12 23:41:32 -0400

    Added some prime generation functions.

--- utils.py
+++ utils.py
@@ -88,3 +88,90 @@ def calc_netmask(mask):
 	for i in xrange(32-mask, 32):
 		bits |= (1 << i)
 	return inet_ntoa(pack('>I', bits))
+
+
+"""
+The following prime generators were taken from this thread:
+	http://groups.google.com/group/comp.lang.python/browse_thread/thread/77cc9764f9d5a072#
+"""
+def prime_gen():
+	"""Generate a list of primes.
+	Faster version would be to move prime_list outside of the function.
+	"""
+    prime_list = [2]
+    for p in prime_list: yield p
+    for n in itertools.count(prime_list[-1] + 1):
+        for p in prime_list:
+            if p * p > n:
+                prime_list.append(n)
+                yield n
+                break
+            elif n % p == 0:
+                break
+        else:
+            raise Exception("Shouldn't have run out of primes!") 
+
+def prime_gen2():
+	"Faster version."
+    yield 2
+    n = 3
+    yield n
+    prime_list = [(2, 4), (3, 9)]
+    it = itertools.count(4)
+    for n in it:
+        n = it.next()
+        for p,p2 in prime_list:
+            if n % p == 0:
+                break
+            elif p2 > n:
+                prime_list.append((n, n*n))
+                yield n
+                break
+        else:
+            raise RuntimeError("Shouldn't have run out of primes!") 
+
+"""
+Another poc for generating primes, thought it looked pretty.
+"""
+from itertools import count
+from math import sqrt
+
+g = (lambda primes = []:
+        (n for n in count(2) if
+            (lambda x, primes:
+                (primes.append(x) or True
+                     if all(x % p for p in primes if p <= sqrt(x))
+                     else False)
+            )(n, primes)
+        )
+    )()
+
+for i in range(500):
+    print g.next()
+
+
+"""
+Here is another speedup (from my prime pair hunt days):
+Check only 2 of every 6 integers.
+[6N + 0, 6N + 2, 6N + 4] are divisible by 2, [6N + 0, 6N + 3] by 3.
+	- Scott David Daniels
+"""
+
+ def prime_gen3():
+	 check_list = [(5, 25)]
+	 yield 2
+	 yield 3
+	 yield 5
+	 n = 5
+	 for incr in itertools.cycle((2, 4)):
+	     n += incr
+	     for p, p_squared in check_list:
+	         if p_squared > n:
+	             check_list.append((n, n * n))
+	             yield n
+	             break
+	         elif n % p == 0:
+	             break
+	     else:
+	         raise Exception("Shouldn't have run out of primes!") 
+	
